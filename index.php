a2616f5ea3c2da416b8d022fa03b9e005a17b080
Priyal Shah <shah.priyal@ymail.com>
2018-10-02 16:40:33 -0700

    Index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include'header.php'; ?>
+<!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <h2>Crypto site</h2>
+            <h4>
+                This website contains demo of various Ciphers & Algorithms<br>
+                Checkout The Demo Section<b><a href="ciphers.php"> here</a></b><br>
+                Free Sign Up to record your history
+            </h4>
+            <ul class="actions">
+                <li><a href="signup.php" class="button big special">Sign Up</a></li>
+                <li><a href="ciphers.php" class="button big alt">Learn More</a></li>
+            </ul>
+            <p></p>
+        </div>
+    </section>
+<?php include'footer.php'; ?>
